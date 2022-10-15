788fb619699e15fb9efa03c78396fa4d0b852e02
Bilal Khilji <kbinfo4u@gmail.com>
2022-10-15 18:50:44 +0530

    Ability to sync uploaded files

--- src/Jobs/SyncLocalUploadJob.php
+++ src/Jobs/SyncLocalUploadJob.php
@@ -49,8 +49,7 @@ class SyncLocalUploadJob implements ShouldQueue
                     foreach ($payLoad->disks as $disk) {
                         $localPath = Storage::disk($payLoad->local_disk)->getDriver()->getAdapter()->getPathPrefix();
                         if ($payLoad->fileName != '' || $payLoad->fileName != null)
-                            \Storage::disk($disk)
-                                ->put($payLoad->remote_path . "/" . $payLoad->fileName,
+                            \Storage::disk($disk)->put($payLoad->remote_path . "/" . $payLoad->fileName,
                                     file_get_contents($localPath . $payLoad->local_path . "/" . $payLoad->fileName));
                     }
                 } catch (\Exception $exception) {
